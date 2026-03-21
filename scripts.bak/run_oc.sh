c2ca2b7b8e40d9d72274ad8667ad12c39f76b661
molmdl <133654677+molmdl@users.noreply.github.com>
2026-03-21 08:47:22 +0800

    chore: repo structure

--- scripts.bak/run_oc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#export BUN_TMPDIR="./tmp"
-#export TMPDIR="./tmp"
-export EDITOR="vim"
-export PATH="/data/nglokwan/opencode/bin/:$PATH"
-
-opencode $1
--- scripts.bak/setenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/data/scripts/gmx2023.5_plumed2.9.3_gpu_shared.env
-source ~/scripts/miniconda_py3.env
-conda activate gmxMMPBSA
