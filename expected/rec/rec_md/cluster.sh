2ad5924747cf2c4da9470ac54c5744ace9865fcd
molmdl <133654677+molmdl@users.noreply.github.com>
2026-03-23 20:20:01 +0800

    chore: update cluster.sh for pdb output

--- expected/rec/rec_md/cluster.sh
+++ expected/rec/rec_md/cluster.sh
@@ -1,2 +1,5 @@
 echo 1 1 | gmx cluster -method gromos -f ./apo_50ns_merged_fit.xtc -s ./pr_0.tpr -rmsmin 0.1 -cutoff 0.2 -sz -clid -cl clusters.xtc
 echo 1 | gmx trjconv -s ./pr_0.tpr -f clusters.xtc -sep -pbc mol -o hsa.gro
+for i in {0..9} ; do
+	gmx editconf -f hsa${i}.gro -o hsa${i}.pdb
+done
