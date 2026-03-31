48c274f84e4b0acad43f8cc17593efcb89762581
molmdl <133654677+molmdl@users.noreply.github.com>
2026-03-31 23:24:58 +0800

    update pr_prod.sh

--- expected/chm/BRD4/scripts/com/pr_prod.sh
+++ expected/chm/BRD4/scripts/com/pr_prod.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-lig=$1 
+cd com
 
-for i in {0..3} ; do
-	sbatch << EOF
+for lig in {1..10} ; do
+	cd lig${lig}
+	for i in {0..3} ; do
+		sbatch << EOF
 #!/bin/bash
-#SBATCH -J ${lig}_100ns_$i
+#SBATCH -J lig${lig}_100ns_$i
 #SBATCH -c 22
 #SBATCH -n 1
-##SBATCH -p rtx4090
-#SBATCH -p workq
+##SBATCH -p rtx4090-short
+#SBATCH -p rtx4090
+##SBATCH -p workq
 #SBATCH --gres=gpu:1
 
 source ~/scripts/gmxMMPBSA.env
@@ -23,4 +26,7 @@ gmx mdrun -deffnm pr_$i -ntmpi 1 -ntomp 22 -bonded gpu -nb gpu -update gpu -pme 
 gmx grompp -f md.mdp -c pr_${i}.gro -p sys.top   -o prod_${i}.tpr -n index.ndx -maxwarn 2
 gmx mdrun -deffnm prod_$i -ntmpi 1 -ntomp 22 -bonded gpu -nb gpu -update gpu -pme gpu -cpt 5	
 EOF
+	done
+	cd ..
 done
+
